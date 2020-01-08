86415e1f8334d1a9e44ba1c81e9e674dc58f8506
Nathan Atherton <nathanatherton2@gmail.com>
2020-01-08 17:59:17 +0000

    Added many more things like league table and stuff

---
+++ components/FetchAPI.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Table from './PremMeritTable'
+
+class FetchData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: [],
+      isLoaded: false,
+    };
+  }
+
+    componentDidMount = () => {
+      this.fetchData();
+    }
+
+    fetchData = () => {
+        fetch('https://v2-api.sheety.co/7f01a568513886dcd760b17376d01421/testing/premMeritsByWins')
+        .then(response => response.json())
+        .then(data => {
+                  this.setState({
+                      isLoaded: true,
+                      result: data.premMeritsByWins,
+                    })
+                  })
+                    .catch(err => console.error(err)
+                  )
+                }
+
+    render() {
+      const { isLoaded } = this.state;
+      if (!isLoaded) {
+        return <h1>Loading the table...</h1>;
+      } else {
+        return (
+        <>
+          <section class="container">
+          <table>
+            <thead>
+              <tr>
+                <th>Position</th>
+                <th>Name</th>
+                <th>Team</th>
+                <th>Played</th>
+                <th>Won</th>
+                <th>Lost</th>
+                <th>Win %</th>
+                <th>8 Balls</th>
+              </tr>
+            </thead>
+                <Table table={this.state.result} />
+          </table>
+          </section>
+        </>
+        )
+    }
+  }
+}
+
+export default FetchData;
---
+++ components/FetchPremTable.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import Table from './PremLeagueTable'
+
+class FetchData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: [],
+      isLoaded: false,
+    };
+  }
+
+    componentDidMount = () => {
+      this.fetchData();
+    }
+
+    fetchData = () => {
+        fetch('https://v2-api.sheety.co/7f01a568513886dcd760b17376d01421/testing/nathanPremTable')
+        .then(response => response.json())
+        .then(data => {
+                  this.setState({
+                      isLoaded: true,
+                      result: data.nathanPremTable,
+                    })
+                    console.log(this.state.result)
+                  })
+                    .catch(err => console.error(err)
+                  )
+                }
+
+    render() {
+      const { isLoaded } = this.state;
+      if (!isLoaded) {
+        return <h1>Loading the table...</h1>;
+      } else {
+        return (
+        <>
+          <section class="container">
+          <table>
+            <thead>
+              <tr>
+                <th>Position</th>
+                <th>Team</th>
+                <th>Played</th>
+                <th>Won</th>
+                <th>Drawn</th>
+                <th>Lost</th>
+                <th>Points</th>
+                <th>Frames Won</th>
+                <th>Frames Lost</th>
+                <th>Frames Difference</th>
+              </tr>
+            </thead>
+            <Table table={this.state.result} />
+          </table>
+          </section>
+        </>
+        )
+    }
+  }
+}
+
+export default FetchData;
