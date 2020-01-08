8f15ac2fea3c2eb9ab16df409a40db27114b8a60
Nathan Atherton <nathanatherton2@gmail.com>
2020-01-08 18:03:11 +0000

    Added build folder

---
+++ build/precache-manifest.58f8028b9f339f0e66fd5c337937e24c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "365b55671dc0015773faf38acbdab498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4070ca35fc5325108faf",
+    "url": "/static/css/main.3a390c95.chunk.css"
+  },
+  {
+    "revision": "454246345cb9a6e0fbf8",
+    "url": "/static/js/2.86289a1a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.86289a1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4070ca35fc5325108faf",
+    "url": "/static/js/main.df2ef2aa.chunk.js"
+  },
+  {
+    "revision": "06f6271ba58b5b0d22ea",
+    "url": "/static/js/runtime-main.896f5687.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
